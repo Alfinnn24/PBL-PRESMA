51c91732e770fe4f64e1eaf87a1a0b1083117264
sukinnamz <innama.maesaputri@gmail.com>
2025-04-30 14:59:44 +0700

    add : migration mahasiswa

---
+++ database/migrations/2025_04_30_075900_create_mahasiswa_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->string('nim')->primary();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('nama_lengkap');
+            $table->year('angkatan');
+            $table->string('no_telp');
+            $table->string('alamat');
+            $table->foreignId('program_studi_id')->constrained('program_studi');
+            $table->text('bidang_keahlian');
+            $table->text('sertifikasi');
+            $table->text('pengalaman');
+            $table->string('foto_profile')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+};
