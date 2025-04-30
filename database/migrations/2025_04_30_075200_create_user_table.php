cae05b9ce829e01471c0cb1d7bcfda9d34777fbd
sukinnamz <innama.maesaputri@gmail.com>
2025-04-30 15:02:34 +0700

    fix : migration user

--- database/migrations/2025_04_30_075200_create_user_table.php
+++ database/migrations/2025_04_30_075200_create_user_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('user');
         Schema::create('user', function (Blueprint $table) {
             $table->id();
             $table->string('username')->unique();
