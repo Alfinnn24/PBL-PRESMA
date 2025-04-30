f04876dcbddbd2477eeb61642bc7e25dedfe77ac
Lelytameyda29rpl <lelyta_meyda_29rpl@student.smktelkom-mlg.sch.id>
2025-04-30 21:48:14 +0700

    add seeder periode

---
+++ database/seeders/PeriodeSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class PeriodeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('periode')->insert([
+            ['nama' => '2024/2025 Ganjil', 'tahun' => 2024, 'semester' => 'Ganjil'],
+            ['nama' => '2024/2025 Genap', 'tahun' => 2025, 'semester' => 'Genap'],
+        ]);
+    }
+}
