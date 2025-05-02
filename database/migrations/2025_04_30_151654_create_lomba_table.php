7139c1bfa4baf931a2d91764c8d13b7ab2bca990
sukinnamz <innama.maesaputri@gmail.com>
2025-05-02 09:41:33 +0700

    fix : lomba migartion and sertifikasi seeder

--- database/migrations/2025_04_30_151654_create_lomba_table.php
+++ database/migrations/2025_04_30_151654_create_lomba_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,7 +16,7 @@ return new class extends Migration
             $table->string('nama');
             $table->string('kategori');
             $table->string('penyelenggara');
-            $table->string('tingkat');
+            $table->enum('tingkat', ['Kota/Kabupaten', 'Provinsi', 'Nasional', 'Internasional']);
             $table->text('bidang_keahlian');
             $table->text('persyaratan');
             $table->string('link_registrasi');
