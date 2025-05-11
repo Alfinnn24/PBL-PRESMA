0f3fb9e0e75ac45049658828af30712b4650bb99
ren5602 <naziz5602@gmail.com>
2025-05-11 15:21:27 +0700

    add tambah user by admin only user  & auth fix

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RekomendasiLombaController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -30,11 +31,18 @@ Route::middleware(['auth'])->group(function () { // artinya semua route di dalam
     Route::get('/rekomendasi', [RekomendasiLombaController::class, 'index'])->name('rekomendasi.index');
     Route::post('/rekomendasi/{id}', [RekomendasiLombaController::class, 'updateStatus'])->name('rekomendasi.updateStatus');
 
-    // Route::get('/login', function () {
-//     return view('auth.login');
-// });
-// Route::get('/register', function () {
-//     return view('auth.register');
-// });
+});
 
+Route::middleware(['authorize:admin'])->group(function () {
+    Route::group(['prefix' => 'user'], function () {
+        Route::get('/', [UserController::class, 'index']);                          // menampilkan halaman awal user
+        Route::post('/list', [UserController::class, 'list']);                      // menampilkan data user dalam bentuk json untuk datatables
+        Route::get('/create_ajax', [UserController::class, 'create_ajax']);         // Menampilkan halaman form tambah user Ajax
+        Route::post('/ajax', [UserController::class, 'store_ajax']);                // Menyimpan data user baru Ajax
+        Route::get('/{id}/show_ajax', [UserController::class, 'show_ajax']);        // menampilkan detail user Ajax
+        Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);        // Menampilkan halaman form edit user Ajax
+        Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);    // Menyimpan perubahan data user Ajax
+        Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);   // Untuk tampilkan form confirm delete user Ajax
+        Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); // Untuk hapus data user Ajax
+    });
 });
