5bd0bc7c9516718e84faabf3930cfd1e74e19d78
ren5602 <naziz5602@gmail.com>
2025-05-13 18:12:39 +0700

    fix bug modal cant closed show detail after decline / acc prestasi from index & rekomendasi modal (not fix)

--- resources/views/admin/prestasi/show_ajax.blade.php
+++ resources/views/admin/prestasi/show_ajax.blade.php
@@ -100,8 +100,8 @@
             </div>
             <div class="modal-footer">
                 <button type="button" data-dismiss="modal" class="btn btn-primary">Tutup</button>
-                <button onclick="ubahStatus({{ $prestasi->id }}, 'approve')" class="btn btn-success btn">Setujui</button>
-                <button onclick="ubahStatus({{ $prestasi->id }}, 'reject')" class="btn btn-danger btn">Tolak</button>
+                <button onclick="ubahStatus({{ $prestasi->id }}, 'approve')" class="btn btn-success">Setujui</button>
+                <button onclick="ubahStatus({{ $prestasi->id }}, 'reject')" class="btn btn-danger">Tolak</button>
             </div>
         </div>
     </div>
