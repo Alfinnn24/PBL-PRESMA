65a49ffedc8ea8611f6e34c9fd3cebf8b5d9e433
sukinnamz <innama.maesaputri@gmail.com>
2025-05-18 22:51:03 +0700

    fix : table rekomendasi dan nambah seeder

--- database/seeders/DetailPrestasiSeeder.php
+++ database/seeders/DetailPrestasiSeeder.php
@@ -25,7 +25,28 @@ class DetailPrestasiSeeder extends Seeder
             ['mahasiswa_nim' => '2341720089', 'prestasi_id' => 8, 'created_at' => now(), 'updated_at' => now()],
             ['mahasiswa_nim' => '2341720042', 'prestasi_id' => 9, 'created_at' => now(), 'updated_at' => now()],
             ['mahasiswa_nim' => '2341720103', 'prestasi_id' => 10, 'created_at' => now(), 'updated_at' => now()],
+            ['mahasiswa_nim' => '2341720172', 'prestasi_id' => 11, 'created_at' => now(), 'updated_at' => now()],
+            ['mahasiswa_nim' => '2341720182', 'prestasi_id' => 12, 'created_at' => now(), 'updated_at' => now()],
+            ['mahasiswa_nim' => '2341720231', 'prestasi_id' => 13, 'created_at' => now(), 'updated_at' => now()],
+            ['mahasiswa_nim' => '2341720256', 'prestasi_id' => 14, 'created_at' => now(), 'updated_at' => now()],
+            ['mahasiswa_nim' => '2341720187', 'prestasi_id' => 15, 'created_at' => now(), 'updated_at' => now()],
+            ['mahasiswa_nim' => '2341720038', 'prestasi_id' => 16, 'created_at' => now(), 'updated_at' => now()],
+            ['mahasiswa_nim' => '2341720238', 'prestasi_id' => 17, 'created_at' => now(), 'updated_at' => now()],
+            ['mahasiswa_nim' => '2341720089', 'prestasi_id' => 18, 'created_at' => now(), 'updated_at' => now()],
+            ['mahasiswa_nim' => '2341720042', 'prestasi_id' => 19, 'created_at' => now(), 'updated_at' => now()],
+            ['mahasiswa_nim' => '2341720103', 'prestasi_id' => 20, 'created_at' => now(), 'updated_at' => now()],
+
+            // Beberapa mahasiswa memiliki prestasi yang sama
+            ['mahasiswa_nim' => '2341720172', 'prestasi_id' => 2, 'created_at' => now(), 'updated_at' => now()],
+            ['mahasiswa_nim' => '2341720182', 'prestasi_id' => 4, 'created_at' => now(), 'updated_at' => now()],
+            ['mahasiswa_nim' => '2341720038', 'prestasi_id' => 8, 'created_at' => now(), 'updated_at' => now()],
+            ['mahasiswa_nim' => '2341720231', 'prestasi_id' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['mahasiswa_nim' => '2341720256', 'prestasi_id' => 6, 'created_at' => now(), 'updated_at' => now()],
+            ['mahasiswa_nim' => '2341720187', 'prestasi_id' => 10, 'created_at' => now(), 'updated_at' => now()],
+            ['mahasiswa_nim' => '2341720089', 'prestasi_id' => 3, 'created_at' => now(), 'updated_at' => now()],
+            ['mahasiswa_nim' => '2341720042', 'prestasi_id' => 5, 'created_at' => now(), 'updated_at' => now()],
+            ['mahasiswa_nim' => '2341720103', 'prestasi_id' => 7, 'created_at' => now(), 'updated_at' => now()],
         ]);
-        
+
     }
 }
