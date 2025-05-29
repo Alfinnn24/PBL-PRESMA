70102f88f3493ca7f72d4be37ab7523b0e07d890
Lelytameyda29rpl <lelyta_meyda_29rpl@student.smktelkom-mlg.sch.id>
2025-05-30 00:21:29 +0700

    perbaikan periode pada menu verifikasi lomba

--- resources/views/admin/verifikasi_lomba/index.blade.php
+++ resources/views/admin/verifikasi_lomba/index.blade.php
@@ -96,7 +96,7 @@
                         width: "40px"
                     },
                     {
-                        data: "periode_id",
+                        data: "periode_display_name",
                         className: "text-center",
                         orderable: true,
                         searchable: true,
